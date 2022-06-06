1821a7f36fb2a7d1ed55b0982a3409c3d52c6a09
fujimura1013 <n.fujimura@ei-shin.com>
2022-06-06 17:30:23 +0900

    TextLabel

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,34 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
+    <TextLabel text="入社" class="label-green" />
+    <TextLabel text="面接待ち" class="label-purple" />
+    <TextLabel text="入社" class="label-blue" />
+    <TextLabel text="面接待ち" class="label-red" />
+    <TextLabel text="面接待ち" class="label-orange" />
   <router-view/>
 </template>
 
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TextLabel from './components/atoms/TextLabel.vue';
+
+export default defineComponent({
+  components: {
+    TextLabel
+  },
+});
+</script>
+
 <style>
+html {
+  font-size: 10px;
+  font-family: Noto Sans JP,Yu-Gothic;
+}
+* {
+  margin: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/atoms/TextLabel.vue
@@ -0,0 +1,54 @@
+<template>
+  <p>{{ text }}</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TextLabel',
+  props:{
+    text: String
+  }
+});
+</script>
+
+<style scoped>
+  p {
+    font-size: 1.4rem;
+    height: 3rem;
+    display: flex;
+    align-items: center;
+    margin: 0 auto;
+    padding: 0 1.2rem;
+    letter-spacing: 0.08em;
+    border-radius: 1.5rem;
+    width: fit-content;
+    box-sizing: border-box;
+  }
+  .label-green{
+    background-color: #1BA664;
+    border: #229A64 solid 1px;
+    color: #fff;
+  }
+  .label-blue{
+    background-color: #1755A6;
+    border: #114993 solid 1px;
+    color: #fff;
+  }
+  .label-red{
+    background-color: #DB675A;
+    border: #CE5B4E solid 1px;
+    color: #fff;
+  }
+  .label-purple{
+    background-color: #E3D7FF;
+    border: #DCD2EF solid 1px;
+    color: #48288B;
+  }
+  .label-orange{
+    background-color: #FFE1BA;
+    border: #F2D8B5 solid 1px;
+    color: #84571C;
+  }
+</style>
