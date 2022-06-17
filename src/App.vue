992a9144285a86eaec481654472dfd2012b2f61d
fujimura1013 <n.fujimura@ei-shin.com>
2022-06-17 15:29:12 +0900

    Heading1 create

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
 <TextLabel text="入社" type="green"/>
+<Heading1 h1="「インターン面接」の回答結果"/>
 </template>
 
 <script lang="ts" setup>
 import TextLabel from './components/atoms/TextLabel.vue';
+import Heading1 from './components/atoms/Heading1.vue';
 </script>
 
 <style>
---
+++ src/components/atoms/Heading1.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ h1 }}</h1>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  h1: String,
+})
+</script>
+
+<style scoped>
+h1 {
+  font-size: 2.9rem;
+  letter-spacing: 0.17em;
+  font-weight: 500;
+}
+</style>
