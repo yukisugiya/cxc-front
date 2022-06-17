d84750faf2dfb3323e6bf1cdd2df7b7255c5054b
fujimura1013 <n.fujimura@ei-shin.com>
2022-06-17 17:35:27 +0900

    TextLabel fix

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
 <template>
+<TextLabel text="入社" type="lightgray"/>
+<TextLabel text="入社" type="gray"/>
+<TextLabel text="入社" type="darkblue"/>
+<TextLabel text="入社" type="blue"/>
+<TextLabel text="入社" type="lightblue"/>
+<TextLabel text="入社" type="darkpurple"/>
+<TextLabel text="入社" type="purple"/>
+<TextLabel text="入社" type="lightpurple"/>
+<TextLabel text="入社" type="lightpink"/>
+<TextLabel text="入社" type="pink"/>
+<TextLabel text="入社" type="red"/>
+<TextLabel text="入社" type="darkorange"/>
+<TextLabel text="入社" type="orange"/>
+<TextLabel text="入社" type="yellow"/>
+<TextLabel text="入社" type="lightgreen"/>
 <TextLabel text="入社" type="green"/>
+<TextLabel text="入社" type="darkgreen"/>
+
 </template>
 
 <script lang="ts" setup>
