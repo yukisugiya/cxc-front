32f5c5467eb0017a116894a1faee1a9e0d3fdebd
fujimura1013 <n.fujimura@ei-shin.com>
2022-06-17 16:49:36 +0900

    Heading1 create

---
+++ src/components/atoms/Heading1.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ h1 }}</h1>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  h1: String,
+})
+</script>
+
+<style scoped>
+h1 {
+  font-size: 2.9rem;
+  color: #3B3C3C;
+  letter-spacing: 1.74px;
+  font-weight: 500;
+}
+</style>
