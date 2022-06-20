4803ae3044db99ed51fbadc200fefc4e0d207f66
fujimura1013 <n.fujimura@ei-shin.com>
2022-06-20 19:16:18 +0900

    TextLabel fix

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,7 @@
 <template>
-<TextLabel text="入社" type="primary"/>
-<TextLabel text="入社" type="gray"/>
-<TextLabel text="入社" type="darkblue"/>
-<TextLabel text="入社" type="blue"/>
-<TextLabel text="入社" type="lightblue"/>
-<TextLabel text="入社" type="darkpurple"/>
-<TextLabel text="入社" type="purple"/>
-<TextLabel text="入社" type="lightpurple"/>
-<TextLabel text="入社" type="lightpink"/>
-<TextLabel text="入社" type="pink"/>
-<TextLabel text="入社" type="red"/>
-<TextLabel text="入社" type="darkorange"/>
-<TextLabel text="入社" type="orange"/>
-<TextLabel text="入社" type="yellow"/>
-<TextLabel text="入社" type="lightgreen"/>
 <TextLabel text="入社" type="green"/>
-<TextLabel text="入社" type="darkgreen"/>
-<Heading1 h1="これはタイトル１です"/>
 </template>
 
 <script lang="ts" setup>
 import TextLabel from './components/atoms/TextLabel.vue';
-import Heading1 from './components/atoms/Heading1.vue';
 </script>
