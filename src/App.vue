a9cd5f52279e82d9b5dd3eefe656b7e6a94d8f35
fujimura1013 <n.fujimura@ei-shin.com>
2022-06-27 11:06:15 +0900

    Heading3 fix

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 <TextLabel text="入社" type="green"/>
 <Heading1 headingText="「インターン面接」の回答結果"/>
 <Heading2 headingText="条件設定" />
+<Heading3 headingText="基本情報で絞り込む" />
 </template>
 
 <script lang="ts" setup>
 import TextLabel from './components/atoms/TextLabel.vue';
 import Heading1 from './components/atoms/Heading1.vue';
 import Heading2 from './components/atoms/Heading2.vue';
+import Heading3 from './components/atoms/Heading3.vue';
 </script>
