1476dded6a86b9fd5db7b756e070b3c4b52e0100
fujimura1013 <n.fujimura@ei-shin.com>
2022-06-29 15:50:06 +0900

    IconHeading create

---
+++ src/components/atoms/IconHeading.vue
@@ -0,0 +1,37 @@
+<template>
+  <h2><i :class="classObject"></i>{{ headingText }}</h2>
+</template>
+
+<script lang="ts" setup>
+import { defineProps, computed } from "vue";
+
+const props = defineProps({
+  headingText: { //表示するテキスト
+    type: String,
+    required: true,
+  },
+  icon: { //アイコンの種類
+    type: String,
+    default: 'search'
+  }
+});
+const classObject = computed(() => ({
+  'fas fa-search': props.icon == 'search'
+}))
+</script>
+
+<style scoped lang="scss">
+h2 {
+  font-size: 2rem;
+  color: #6e6e6e;
+  letter-spacing: 1.2px;
+  font-weight: 500;
+
+  i {
+    position: relative;
+    bottom: 1.5px;
+    font-size: 1.4rem;
+    margin-right: 0.5rem;
+  }
+}
+</style>
