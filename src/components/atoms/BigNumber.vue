5f5df1426d6d10602c6db9390c2215a088c085d0
fujimura1013 <n.fujimura@ei-shin.com>
2022-06-29 15:43:43 +0900

    BigNumber create

---
+++ src/components/atoms/BigNumber.vue
@@ -0,0 +1,37 @@
+<template>
+  <p><em :class="classObject">{{ num }}</em>件</p>
+</template>
+
+<script lang="ts" setup>
+import { defineProps, computed } from 'vue';
+
+const props = defineProps({
+  //件数の数字
+  num: {
+    type: Number,
+    required: true
+  }
+})
+//numが0以上の時に色が変わる処理
+const classObject = computed(() => props.num > 0 ? 'active' : '')
+
+</script>
+
+<style lang="scss" scoped>
+p {
+  font-size: 1.2rem;
+  color: #6E6E6E;
+  letter-spacing: 0.72px;
+  font-weight: 400;
+}
+em {
+  font-size: 3rem;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 600;
+  margin-right: 0.2rem;
+  color: #E2E2E2;
+  &.active{
+    color: #3B3C3C;
+  }
+}
+</style>
