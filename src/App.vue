2391dc4f2439da8787ac9c2a779e8605fbc2defd
fujimura1013 <n.fujimura@ei-shin.com>
2022-07-04 12:40:33 +0900

    create LinkText

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
 <TextLabel text="入社" type="green"/>
+<LinkText text="入社してみる"/>
 </template>
 
 <script lang="ts" setup>
 import TextLabel from './components/atoms/TextLabel.vue';
+import LinkText from './components/atoms/LinkText.vue';
 </script>
---
+++ src/components/atoms/LinkText.vue
@@ -0,0 +1,49 @@
+<template>
+  <button>
+    <i :class="iconName"></i>
+    {{ text }}
+  </button>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  text: {
+    type: String,
+    required: true
+  },
+  icon: {
+    type: String,
+    default: 'chevron-left'
+  }
+});
+
+const iconName = 'fas fa-' + props.icon;
+</script>
+
+<style lang="scss" scoped>
+button {
+  color: #6e6e6e;
+  font-size: 1.4rem;
+  font-weight: 500;
+  letter-spacing: 0.84px;
+  border-bottom: #6e6e6e solid 1px;
+  padding-bottom: 1px;
+  position: relative;
+  margin-left: 1.5rem;
+  cursor: pointer;
+  transition: 0.2s;
+
+  i {
+    position: absolute;
+    font-size: 1.1rem;
+    left: -1.5rem;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  &:hover {
+    opacity: 0.5;
+  }
+}
+</style>
