db78434debc0460cf771f57a8d43f3776936ca82
fujimura1013 <n.fujimura@ei-shin.com>
2022-07-04 17:18:17 +0900

    create Hamburger

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
 <TextLabel text="入社" type="green"/>
+<Hamburger />
 </template>
 
 <script lang="ts" setup>
 import TextLabel from './components/atoms/TextLabel.vue';
+import Hamburger from './components/atoms/Hamburger.vue';
 </script>
---
+++ src/components/atoms/Hamburger.vue
@@ -0,0 +1,25 @@
+<template>
+  <button type="button">
+    <i class="fas fa-bars"></i>
+  </button>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from "vue";
+
+const props = defineProps({});
+</script>
+
+<style lang="scss" scoped>
+button {
+  i {
+    font-size: 1.6rem;
+    color: #484848;
+    transition: 0.2s;
+
+    &:hover {
+      opacity: 0.5;
+    }
+  }
+}
+</style>
