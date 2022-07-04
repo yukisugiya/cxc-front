6c3a4e17f0f3a365ae8d2ea7aa953207bcfc7dd1
fujimura1013 <n.fujimura@ei-shin.com>
2022-07-04 17:02:26 +0900

    create DiscText

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
 <TextLabel text="入社" type="green"/>
+<DiscText text="分析結果"/>
 </template>
 
 <script lang="ts" setup>
 import TextLabel from './components/atoms/TextLabel.vue';
+import DiscText from './components/atoms/DiscText.vue';
 </script>
---
+++ src/components/atoms/DiscText.vue
@@ -0,0 +1,48 @@
+<template>
+  <p>{{ text }}</p>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  //表示するテキスト
+  text: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+p {
+  font-size: 1.3rem;
+  font-weight: 400;
+  letter-spacing: 0.78px;
+  color: #929292;
+  position: relative;
+  padding-left: 1rem;
+  transition: 0.2s;
+  cursor: pointer;
+
+  &::before {
+    content: "";
+    height: 6px;
+    width: 6px;
+    border-radius: 50%;
+    background-color: #929292;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    left: 0;
+  }
+  &:hover,
+  &.active {
+    color: #3b3c3c;
+
+    &::before {
+      background-color: #3b3c3c;
+    }
+  }
+}
+</style>
