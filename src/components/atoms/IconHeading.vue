2418afa157ee7496a7add53bbf22e858a7881e90
fujimura1013 <n.fujimura@ei-shin.com>
2022-07-08 13:44:14 +0900

    IconHeading fix

--- src/components/atoms/IconHeading.vue
+++ src/components/atoms/IconHeading.vue
@@ -6,30 +6,32 @@
 import { defineProps, computed } from "vue";
 
 const props = defineProps({
-  headingText: { //表示するテキスト
+  headingText: {
+    //表示するテキスト
     type: String,
     required: true,
   },
-  icon: { //アイコンの種類
+  icon: {
+    //アイコンの種類
     type: String,
-    default: 'search'
-  }
+    default: "search",
+  },
 });
-
-const iconName = 'fas fa-' + props.icon;
+const iconName = computed(() => "fas fa-" + props.icon);
 </script>
 
 <style scoped lang="scss">
+@import "./src/assets/scss/global";
 h2 {
-  font-size: 2rem;
-  color: #6e6e6e;
+  font-size: $x-large;
+  $gray: #6e6e6e;
   letter-spacing: 1.2px;
-  font-weight: 500;
+  font-weight: $medium;
 
   i {
     position: relative;
     bottom: 1.5px;
-    font-size: 1.4rem;
+    font-size: $default;
     margin-right: 0.5rem;
   }
 }
