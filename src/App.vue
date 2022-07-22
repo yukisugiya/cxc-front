3b3cccd0b387c984f65b5719a5c0058226aed833
fujimura1013 <n.fujimura@ei-shin.com>
2022-07-22 10:28:47 +0900

    App.vue fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-<DiscTextLink text="分析結果" url="https://trello.com/b/gNa2mRxp/cxc"/>
+
 </template>
 
 <script lang="ts" setup>
-import DiscTextLink from './components/atoms/DiscTextLink.vue';
+
 </script>
--- src/components/atoms/DiscTextLink.vue
+++ src/components/atoms/DiscTextLink.vue
@@ -13,6 +13,7 @@ defineProps({
   },
   url: {
     type: String,
+    required: true,
   }
 });
 </script>
