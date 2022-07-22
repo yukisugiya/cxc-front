619d4621a5897694317e3061cd28d74e6f096fc9
fujimura1013 <n.fujimura@ei-shin.com>
2022-07-22 13:11:26 +0900

    create SquareText

---
+++ src/components/atoms/SquareText.vue
@@ -0,0 +1,41 @@
+<template>
+  <p>
+    <span v-if="(square == true)">■</span>
+    {{ text }}
+    <span v-if="(required == true)" class="required">*</span>
+  </p>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from "vue";
+
+defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+  square: {
+    type: Boolean,
+    default: true
+  },
+  required: {
+    type: Boolean,
+    default: true
+  }
+});
+</script>
+
+<style scoped lang="scss">
+@import "./src/assets/scss/global";
+
+p{
+  letter-spacing: $wide-spacing;
+  font-size: $large;
+  color: $navy;
+  font-weight: $medium;
+
+  & .required{
+    color: $orange;
+  }
+}
+</style>
