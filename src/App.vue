f5e0b86645fa26cfb706679c29b9d2f158539d66
fujimura1013 <n.fujimura@ei-shin.com>
2022-07-22 10:20:09 +0900

    App.vue fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-<LinkText text="入社してみる" url="https://github.com/yukisugiya/cxc-front/pull/25"/>
+
 </template>
 
 <script lang="ts" setup>
-import LinkText from './components/atoms/LinkText.vue';
+
 </script>
