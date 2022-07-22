561a1fde2694c1a909a71978a6ddca8194f2d1da
fujimura1013 <n.fujimura@ei-shin.com>
2022-07-22 10:17:49 +0900

    App.vue fix

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
-<ButtonBasic text="削除" color="cta" type="submit" icon="times" :disabled="false" @click-event="aaaa"/>
+
 </template>
 
 <script lang="ts" setup>
-import ButtonBasic from './components/atoms/ButtonBasic.vue';
 
-const aaaa = () => {alert('hello')}
 </script>
