47e6dad8da81ad5e84c7876f389c001ebb7b0f1a
fujimura1013 <n.fujimura@ei-shin.com>
2022-07-22 16:15:12 +0900

    IconTip create

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-
+<IconTip tipText="求職者情報を確認する" url="#"/>
+<IconTip tipText="求職者情報を確認する" url="#"/>
+<IconTip tipText="求職者情報を確認する" url="#"/>
 </template>
 
 <script lang="ts" setup>
-
+import IconTip from './components/atoms/IconTip.vue';
 </script>
