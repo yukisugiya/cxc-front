c81e68879f9e6494b8c4c7e421efcb9e7eb259b2
fujimura1013 <n.fujimura@ei-shin.com>
2022-07-25 15:06:35 +0900

    App.vue 編集

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
-<Hamburger @Hamburger-event="aa" />
+
 </template>
 
 <script lang="ts" setup>
-import Hamburger from './components/atoms/Hamburger.vue';
-const aa = () => {
-    console.log(9)
-}
+
 </script>
